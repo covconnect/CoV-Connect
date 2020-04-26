c7da827f5fe0f057558891232652eebec677256b
jlissner <jlissner@gmail.com>
2020-04-26 10:31:17 -0500

    updated styles on sidebar

--- src/frontend/src/components/layout/Sidebar.js
+++ src/frontend/src/components/layout/Sidebar.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { NavLink } from 'react-router-dom';
 import { slide as Menu } from 'react-burger-menu';
 import Logout from "./Logout.js";
+import './sidebar.css';
 
 // HAMBURGER NAV ITEMS (HOME)
 // Used this object structure to create the sidebar menu items:
@@ -21,20 +22,18 @@ function Sidebar({ items }) {
       <Menu>
         {items.map(({ title, contents }) => (
           <div className="hamburger-nav">
-            {title}
+            <span className="bm-title">{title}</span>
             {contents.map(({ destination, label }) => (
-              <li>
-                <NavLink
-                  key={label}
-                  className="menu-item"
-                  to={destination}
-                  style={{
-                    cursor: "pointer",
-                  }}
-                >
-                  {label}
-                </NavLink>
-              </li>
+              <NavLink
+                key={label}
+                className="menu-item"
+                to={destination}
+                style={{
+                  cursor: "pointer",
+                }}
+              >
+                {label}
+              </NavLink>
             ))}{" "}
           </div>
         ))}
